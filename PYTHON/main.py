f480a86e5f3da9662a1d0e0bc21e82871b023ad6
Jacky <oechslein@gmx.de>
2021-04-23 21:33:48 +0200

    Added pyinstaller task to Python

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,26 +1,34 @@
 from typing import Optional
 
+import requests
+import uvicorn
 from fastapi import Depends, Request
 from fastapi import FastAPI, File, UploadFile
 from fastapi.security.api_key import APIKey
 
 from authorization import get_api_key
 
-import requests
+#import requests
 
-with open(r'c:\temp\foo.gcode', 'rb') as file:
-    result = requests.post('http://127.0.0.1:10000/api/files/local?apikey=123456', files={'file': ('foo.gcode', file)})
-    print(result)
-
-quit()
+#with open(r'c:\temp\foo.gcode', 'rb') as file:
+#    result = requests.post('http://127.0.0.1:10000/api/files/local?apikey=123456', files={'file': ('foo.gcode', file)})
+#    print(result)#
+#
+#quit()
 
 app = FastAPI()
 
 
 @app.post("/api/files/local")
 def receive_file(request: Request, file: UploadFile = File(...), api_key: APIKey = Depends(get_api_key)):
-    result = requests.post(f'http://192.168.178.69/rr_upload?name=gcodes/{file.filename}', data=file.file)
+    reprap_server = '192.168.178.69'
+    print(f'Received gcode file {file.filename}, forwarding to RepRap Server {reprap_server}.')
+    result = requests.post(f'http://{reprap_server}/rr_upload?name=gcodes/{file.filename}', data=file.file)
     file_uploaded = result.status_code == 200 and result.json().get("err", None) == 0
+    if file_uploaded:
+        print(f'Forwarding successfully to to RepRap Server')
+    else:
+        print(f'Problems while forwarding to to RepRap Server')
     return {
         "files": {
             "local": {
@@ -43,3 +51,11 @@ def version():
         "server": "1.3.10",
         "text": "OctoPrint 1.3.10"
     }
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        app,
+        host='127.0.0.1',
+        port=80,
+    )
